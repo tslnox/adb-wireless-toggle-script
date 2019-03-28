796d2acf26e3264734d5cfcfc20dc733fbb3e347
Stanislav Sobotka <tslnox@gmail.com>
2019-03-28 13:31:59 +0100

    Fixed permissions on adbw script

--- config.sh
+++ config.sh
@@ -85,6 +85,7 @@ set_permissions() {
 
   # The following is default permissions, DO NOT remove
   set_perm_recursive  $MODPATH  0  0  0755  0644
+  set_perm $MODPATH/system/xbin/adbw 0 2000 0755
 }
 
 ##########################################################################################
